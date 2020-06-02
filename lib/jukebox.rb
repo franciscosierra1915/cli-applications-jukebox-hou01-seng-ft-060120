627503f1752a8a1b908e819b98e1cfdaa1054491
Francisco Sierra <fransierra.s.15@gmail.com>
2020-06-02 16:13:59 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -32,7 +32,7 @@ def play(songs)
     if users_song == val
       puts "Playing #{val}"
     end
-    if users_song == index+1 
+    if users_song == index 
       puts "Playing #{val}"
     end
   end
