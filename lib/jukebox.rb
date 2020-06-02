828ecd9c1b9626f0a27b4b378b1ca815356ca454
Francisco Sierra <fransierra.s.15@gmail.com>
2020-06-02 16:01:57 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -30,7 +30,7 @@ def play(songs)
   users_song = gets.strip
   
   songs.each_with_index do |val, index|
-    if users_song index+1 == || val == users_song
+    if users_song index+1 == || users_song == val
       puts "Playing #{val}"
     else 
       puts "Invalid input, please try again"
